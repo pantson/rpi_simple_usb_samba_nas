9a50072c0608bfe5fbf5ba19f2496562cac9c1f9
rich <35967917+pantson@users.noreply.github.com>
2021-03-25 05:55:22 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
-wget https://raw.githubusercontent.com/pantson/rpi_simple_usb_samba/main/mount_and_share_uxb.sh
+
+# download file
+wget -O mount_and_share_uxb.sh https://raw.githubusercontent.com/pantson/rpi_simple_usb_samba/main/mount_and_share_uxb.sh
+
+# execute rights
 chmod +x mount_and_share_uxb.sh
+
+# edit /etc/rc.local
 grep -qxF 'sudo bash /home/pi/mount_and_share_uxb.sh' /etc/rc.local || sudo awk '/^exit 0/ { print "sudo bash /home/pi/mount_and_share_uxb.sh"}1' /etc/rc.local > /tmp/rc.local
 if [ -f "/tmp/rc.local" ]; then
   mv /tmp/rc.local /etc/rc.local
 fi
-echo Rebooting...
 
+# reboot
+echo Rebooting...
+shutdown -r now
