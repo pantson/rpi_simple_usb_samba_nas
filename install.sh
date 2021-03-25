a5e322579146d91a642557df60c4bec8ad217ca6
rich <35967917+pantson@users.noreply.github.com>
2021-03-25 06:20:38 +0000

    Update mount_and_share_uxb.sh (#1)
    update install and readme

---
+++ install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# This software is provided 'as-is', without any express or implied
+# warranty.  In no event will the authors be held liable for any damages
+# arising from the use of this software.
+#
+# Permission is granted to anyone to use this software for any purpose,
+# including commercial applications, and to alter it and redistribute it
+# freely, subject to the following restrictions:
+#
+# 1. The origin of this software must not be misrepresented; you must not
+# claim that you wrote the original software. If you use this software
+# in a product, an acknowledgment in the product documentation would be
+# appreciated but is not required.
+# 2. Altered source versions must be plainly marked as such, and must not be
+# misrepresented as being the original software.
+# 3. This notice may not be removed or altered from any source distribution.
+
+# download file
+wget -O mount_and_share_uxb.sh https://raw.githubusercontent.com/pantson/rpi_simple_usb_samba/main/mount_and_share_uxb.sh
+
+# execute rights
+chmod +x mount_and_share_uxb.sh
+
+# edit /etc/rc.local
+grep -qxF 'sudo bash /home/pi/mount_and_share_uxb.sh' /etc/rc.local || sudo awk '/^exit 0/ { print "sudo bash /home/pi/mount_and_share_uxb.sh"}1' /etc/rc.local > /tmp/rc.local
+if [ -f "/tmp/rc.local" ]; then
+  mv /tmp/rc.local /etc/rc.local
+fi
+
+# reboot
+echo Rebooting...
+shutdown -r now
--- mount_and_share_uxb.sh
+++ mount_and_share_uxb.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
 
+# This software is provided 'as-is', without any express or implied
+# warranty.  In no event will the authors be held liable for any damages
+# arising from the use of this software.
+#
+# Permission is granted to anyone to use this software for any purpose,
+# including commercial applications, and to alter it and redistribute it
+# freely, subject to the following restrictions:
+#
+# 1. The origin of this software must not be misrepresented; you must not
+# claim that you wrote the original software. If you use this software
+# in a product, an acknowledgment in the product documentation would be
+# appreciated but is not required.
+# 2. Altered source versions must be plainly marked as such, and must not be
+# misrepresented as being the original software.
+# 3. This notice may not be removed or altered from any source distribution.
+
 # automounts any usb drive in raspberry pi
 # creates public readonly samba share for each folder on each usn stick under /[usb]/shares/
 # eg /usb1/shares/music is shared as music
 
 # requires samba installing
-# sudo apt-get install samba
+echo "samba-common samba-common/workgroup string  WORKGROUP" | sudo debconf-set-selections
+echo "samba-common samba-common/dhcp boolean false" | sudo debconf-set-selections
+echo "samba-common samba-common/do_debconf boolean true" | sudo debconf-set-selections
+apt-get install samba ntfs-3g fuse -y -qq
 
 # place line in /etc/rc.local to call this bash script on startup
 # eg sudo bash /home/pi/mount_and_share_uxb.sh
@@ -13,12 +32,12 @@
 SAMBA=/etc/samba/smb.conf
 
 # make backup of samba config
-if ! [ -f "$SAMBA" ]; then
-    cp $SAMBA /etc/samba/smb.conf.orig
+if ! [ -f "$SAMBA.orig" ]; then
+    cp $SAMBA $SAMBA.orig
 fi
 
 # restore samba config
-cp /etc/samba/smb.conf.orig $SAMBA
+cp $SAMBA.orig $SAMBA
 
 for value in {1..9}
 do
@@ -31,7 +50,7 @@ if [ -b "$DIR" ]; then
     fi
         mount $DIR $USB
     if [ -d "$USB/shares" ]; then
-       for dir in "$USB/shares/*"; do
+       for dir in $(ls "$USB/shares"); do
             SHARE=$(basename $dir)
             echo Sharing $SHARE
             echo [$SHARE]>>$SAMBA
@@ -45,3 +64,4 @@ if [ -b "$DIR" ]; then
 fi
 done
 service smbd restart
+
